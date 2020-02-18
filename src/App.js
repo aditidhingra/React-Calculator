cc01fb17d061e8daf0fc019b455902921ae11841
aditidhingra <60363062+aditidhingra@users.noreply.github.com>
2020-02-18 21:34:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './App.css';
+import KeyPad from './components/KeyPad';
+import Output from './components/Output';
+
+class App extends Component {
+  state={
+    result: ''
+  }
+  buttonPressed = buttonName =>{
+    if(buttonName=='='){
+      this.calculate()
+    }
+    else
+    if(buttonName=='C'){
+      this.reset()
+    } else if(buttonName=='CE'){
+      this.backspace()
+    }else
+    this.setState({
+      result: this.state.result+buttonName
+    });
+  };
+
+  backspace = () =>{
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    });
+  };
+
+  reset = () =>{
+    this.setState({
+      result: ""
+    });
+  };
+
+  calculate = () =>{
+    try{
+    this.setState({
+      result: (eval(this.state.result) || "") + ""
+    });
+  }catch(e){
+    this.setState({
+      result: "error"
+    });
+  }
+  };
+  render(){
+  return (
+    <div className="App">
+    <div className="calc-body">
+    <Output result={this.state.result}></Output>
+    <KeyPad buttonPressed={this.buttonPressed}></KeyPad>
+    </div>
+    </div>
+  );
+}
+}
+export default App;
